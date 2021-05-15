f3dc4192df13d52a66c123e8525965de70e56532
Caleb201 <anyaechecaleb00@gmail.com>
2021-05-14 22:59:44 -0500

    Corrected data spliting.

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -32,6 +32,9 @@
 # + [markdown] id="EpFGOWLv208v"
 # # Sentiment Analysis on Amazon Commerce Reviews - TensorFlow
 
+# + [markdown] id="oKUIypPNr7pb"
+# Team Memebers: Caleb Anyaeche & Matthew Zlibut 
+
 # + [markdown] id="A-Odp34Y208w"
 # ### Background
 # Sentiment Analysis is a technique used in order to understand the emotional tone behind a series of words. It is used for many different purposes such as gaining an idea of public opinion on certain topics, used to understand customer reviews, for market research, and customer service approaches. There are many nuances in the english language, grammer, slang, cultural variation, and tone can change a review in a way that is hard for a machine to understand. Consider the sentence "My package was delayed. Brilliant!"- most humans would recognize this as sarcasm, but the machine may see "Brilliant!" and decide that this is a positive review. Our goal is to try and maximize the machines accuracy using a Recurrent Neural Network.
@@ -49,7 +52,7 @@
 # Independent - Text (string of sentences). 
 # Dependent -Label (Negative 0, Positive 1)
 
-# + colab={"base_uri": "https://localhost:8080/"} id="kI2zqvs_GUWV" executionInfo={"status": "ok", "timestamp": 1620962020438, "user_tz": 300, "elapsed": 208, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="a3be9b4e-4f7b-4523-ff21-e98e9ba36546"
+# + colab={"base_uri": "https://localhost:8080/"} id="kI2zqvs_GUWV" executionInfo={"status": "ok", "timestamp": 1621044136378, "user_tz": 300, "elapsed": 18470, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="e3cdc832-2812-4b17-ad7f-6cfc78f66e54"
 using_colab = True 
 from google.colab import drive 
 drive.mount('/content/drive')
@@ -57,13 +60,13 @@ drive.mount('/content/drive')
 #using_colab = False 
 
 
-# + colab={"base_uri": "https://localhost:8080/"} id="j3t5cYT5QVFv" executionInfo={"status": "ok", "timestamp": 1620962022584, "user_tz": 300, "elapsed": 197, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="7e3c120d-f4e3-44ee-824e-e34b96a6bdc2"
+# + colab={"base_uri": "https://localhost:8080/"} id="j3t5cYT5QVFv" executionInfo={"status": "ok", "timestamp": 1621044144956, "user_tz": 300, "elapsed": 265, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="2359923c-9d75-4a20-9565-dd1e9c0b2077"
 # %cd "/content/drive/MyDrive/Colab Notebooks"
 
-# + colab={"base_uri": "https://localhost:8080/"} id="t4Q9V2wfRBAJ" executionInfo={"status": "ok", "timestamp": 1620962024376, "user_tz": 300, "elapsed": 213, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="5fb3db8a-ec90-4b7e-d99c-15933853a8ac"
+# + colab={"base_uri": "https://localhost:8080/"} id="t4Q9V2wfRBAJ" executionInfo={"status": "ok", "timestamp": 1621044146049, "user_tz": 300, "elapsed": 261, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="e50a78af-db20-43f1-afce-883d00286a8b"
 # %set_env TFDS_DATA_DIR=/content/drive/MyDrive/Colab Notebooks/tfds/
 
-# + id="nJEnCR73nX9l" executionInfo={"status": "ok", "timestamp": 1620962025801, "user_tz": 300, "elapsed": 174, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="nJEnCR73nX9l"
 #Import necessary packages 
 
 import re
@@ -80,7 +83,7 @@ from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import GRU
 from tensorflow.keras.layers import Dense
 
-# + colab={"base_uri": "https://localhost:8080/", "height": 204} id="luyebS0Q_vDy" executionInfo={"status": "ok", "timestamp": 1620962028315, "user_tz": 300, "elapsed": 1032, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="47f25c0f-cc94-40e1-e8ca-38fbe8706234"
+# + colab={"base_uri": "https://localhost:8080/", "height": 204} id="luyebS0Q_vDy" executionInfo={"status": "ok", "timestamp": 1621044153498, "user_tz": 300, "elapsed": 2587, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="8303ed07-b492-4433-f093-844ba6d7584c"
 if using_colab:
     df = pd.read_csv('./Data/amazon_reviews.csv')
 else:
@@ -89,7 +92,7 @@ else:
 df.head()
 
 
-# + id="xExhCTu39ISA" executionInfo={"status": "ok", "timestamp": 1620962040121, "user_tz": 300, "elapsed": 7143, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="xExhCTu39ISA"
 #Preprocessing 
 
 def remove_numbers(x):
@@ -102,7 +105,7 @@ def remove_numbers(x):
 #Removing Numbers
 df['text'] = df['text'].apply(lambda x: remove_numbers(x)) 
 
-# + colab={"base_uri": "https://localhost:8080/"} id="ngXLEK0ll_dM" executionInfo={"status": "ok", "timestamp": 1620962049249, "user_tz": 300, "elapsed": 660, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="b9739c1a-8600-46b0-c711-31bb8541cb39"
+# + colab={"base_uri": "https://localhost:8080/"} id="ngXLEK0ll_dM" executionInfo={"status": "ok", "timestamp": 1621044173398, "user_tz": 300, "elapsed": 476, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="944510fe-34c9-4d72-a837-3a8b4eaadb39"
 # Create a dataset
 
 target = df.pop('label')
@@ -117,21 +120,21 @@ for ex in ds_raw.take(3):
 # + [markdown] id="0T2tCwX_pJdH"
 # TRAIN, VALIDATION, TEST SPLITS
 
-# + id="K6HeiEkcpNbm" executionInfo={"status": "ok", "timestamp": 1620962056994, "user_tz": 300, "elapsed": 207, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="K6HeiEkcpNbm"
 tf.random.set_seed(1)
 
 ds_raw = ds_raw.shuffle(
-    50000, reshuffle_each_iteration=False)
+    150000, reshuffle_each_iteration=False)
 
-ds_raw_test = ds_raw.take(25000)
-ds_raw_train_valid = ds_raw.skip(25000)
-ds_raw_train = ds_raw_train_valid.take(20000)
-ds_raw_valid = ds_raw_train_valid.skip(20000)
+ds_raw_test = ds_raw.take(75000)
+ds_raw_train_valid = ds_raw.skip(75000)
+ds_raw_train = ds_raw_train_valid.take(37000)
+ds_raw_valid = ds_raw_train_valid.skip(37000)
 
 # + [markdown] id="3afOxC8zppNb"
 # TOKENIZER
 
-# + colab={"base_uri": "https://localhost:8080/"} id="8fMYEip4puID" executionInfo={"status": "ok", "timestamp": 1620962063586, "user_tz": 300, "elapsed": 4208, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="d035093c-f6cf-4e1a-b993-74cabb5ea9cf"
+# + colab={"base_uri": "https://localhost:8080/"} id="8fMYEip4puID" executionInfo={"status": "ok", "timestamp": 1621044964227, "user_tz": 300, "elapsed": 7684, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="1273666e-79e7-43a7-ea4c-6a48fc6dc6ff"
 ## find unique tokens (words)
 
 tokenizer = tfds.deprecated.text.Tokenizer()
@@ -143,7 +146,7 @@ for example in ds_raw_train:
     
 print('Vocab-size:', len(token_counts))
 
-# + colab={"base_uri": "https://localhost:8080/"} id="Yd5xlJPXtMod" executionInfo={"status": "ok", "timestamp": 1620962065746, "user_tz": 300, "elapsed": 302, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="43737e96-62e1-4af0-ed04-58872ebe5c2e"
+# + colab={"base_uri": "https://localhost:8080/"} id="Yd5xlJPXtMod" executionInfo={"status": "ok", "timestamp": 1621044968906, "user_tz": 300, "elapsed": 338, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="498d9714-7367-42f8-8921-e2b26ac52c53"
 ## Encoding each unique token into integers
 
 encoder = tfds.deprecated.text.TokenTextEncoder(token_counts)
@@ -152,7 +155,7 @@ example_str = 'This is an example!'
 encoder.encode(example_str)
 
 
-# + id="N6K115AJtXRp" executionInfo={"status": "ok", "timestamp": 1620962071784, "user_tz": 300, "elapsed": 203, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="N6K115AJtXRp"
 ## Define the function for transformation
 
 def encode(text_tensor, label):
@@ -166,7 +169,7 @@ def encode_map_fn(text, label):
                           Tout=(tf.int64, tf.int64))
 
 
-# + colab={"base_uri": "https://localhost:8080/"} id="s3cbJ2VAth97" executionInfo={"status": "ok", "timestamp": 1620962074059, "user_tz": 300, "elapsed": 1051, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="f72a748a-e28a-4516-df53-ab643ada01f0"
+# + colab={"base_uri": "https://localhost:8080/"} id="s3cbJ2VAth97" executionInfo={"status": "ok", "timestamp": 1621044982555, "user_tz": 300, "elapsed": 1126, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="5517716f-81c7-4d5c-bcfc-23e2444342e8"
 ds_train = ds_raw_train.map(encode_map_fn)
 ds_valid = ds_raw_valid.map(encode_map_fn)
 ds_test = ds_raw_test.map(encode_map_fn)
@@ -174,10 +177,9 @@ ds_test = ds_raw_test.map(encode_map_fn)
 tf.random.set_seed(1)
 for example in ds_train.shuffle(1000).take(5):
     print('Sequence length:', example[0].shape)
+#example
 
-example
-
-# + id="EXeYLPn1uOEm" executionInfo={"status": "ok", "timestamp": 1620962078446, "user_tz": 300, "elapsed": 236, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="EXeYLPn1uOEm"
 ## batching the datasets
 train_data = ds_train.padded_batch(
     32, padded_shapes=([-1],[]))
@@ -192,7 +194,7 @@ test_data = ds_test.padded_batch(
 # + [markdown] id="E3MnAIbkwND0"
 # BUILD RNN MODEL
 
-# + id="8Wl6uhEqwPqI" executionInfo={"status": "ok", "timestamp": 1620962081971, "user_tz": 300, "elapsed": 198, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}}
+# + id="8Wl6uhEqwPqI"
 def build_rnn_model(embedding_dim, vocab_size,
                     recurrent_type='SimpleRNN',
                     n_recurrent_units=64,
@@ -243,20 +245,20 @@ def build_rnn_model(embedding_dim, vocab_size,
 
 
 
-# + colab={"base_uri": "https://localhost:8080/"} id="rejTqqOAxEIn" executionInfo={"status": "ok", "timestamp": 1620962085988, "user_tz": 300, "elapsed": 453, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="7a9139e8-01dc-468f-c191-9d00db3f6918"
+# + colab={"base_uri": "https://localhost:8080/"} id="rejTqqOAxEIn" executionInfo={"status": "ok", "timestamp": 1621049774568, "user_tz": 300, "elapsed": 820, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="09795024-2045-43c3-9d8e-40ae15d636bf"
 embedding_dim = 20
 vocab_size = len(token_counts) + 2
 
 rnn_model = build_rnn_model(
     embedding_dim, vocab_size,
-    recurrent_type='SimpleRNN', 
+    recurrent_type='LSTM', 
     n_recurrent_units=64,
     n_recurrent_layers=1,
     bidirectional=True)
 
 rnn_model.summary()
 
-# + colab={"base_uri": "https://localhost:8080/", "height": 392} id="r5FwUWXnynhh" executionInfo={"status": "error", "timestamp": 1620962116340, "user_tz": 300, "elapsed": 15700, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="939b423c-3608-4f6b-a37d-7c7c94a5d4c9"
+# + colab={"base_uri": "https://localhost:8080/"} id="r5FwUWXnynhh" executionInfo={"status": "ok", "timestamp": 1621050571346, "user_tz": 300, "elapsed": 435130, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="82044a98-96f6-4482-d9a7-19d6548b7b74"
 rnn_model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
                   metrics=['accuracy'])
@@ -265,9 +267,9 @@ rnn_model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
 history = rnn_model.fit(
     train_data, 
     validation_data=valid_data, 
-    epochs=10)
+    epochs=3)
 
-# + id="9FeMYgOZy91f"
+# + id="9FeMYgOZy91f" colab={"base_uri": "https://localhost:8080/"} executionInfo={"status": "ok", "timestamp": 1621050952501, "user_tz": 300, "elapsed": 120474, "user": {"displayName": "Caleb Anyaeche", "photoUrl": "", "userId": "02380624916791193636"}} outputId="8b67f471-686f-4f7b-dedf-600bba1fe3c1"
 results = rnn_model.evaluate(test_data)
 print('Test Acc.: {:.2f}%'.format(results[1]*100))
 
@@ -279,6 +281,15 @@ print('Test Acc.: {:.2f}%'.format(results[1]*100))
 # 3. A RNN with 1 LSTM Layer and 1 epochs: 87.04%
 # 4. A RNN with 2 GRU Layer and 5 epochs: 84.48%
 # 5. A RNN with 2 LSTM Layer and 1 epochs: 86.36%
+#
+# NEW ACCURACY FOR EACH MODEL (with corrected data splitting)
+# 1. A Simple RNN and 2 epochs: 
+# 2. A RNN with 1 GRU Layer and 3 epochs: 87.13
+# 3. A RNN with 1 LSTM Layer and 1 epochs: 88.02
+# 4. A RNN with 2 GRU Layer and 5 epochs: 
+# 5. A RNN with 2 LSTM Layer and 1 epochs: 88.9
+# 6. A RNN with 1 GRU Layer and 1 epochs: 88.02
+# 7. A RNN with 1 LSTM Layer and 3 epochs: 85.73%
 
 # + [markdown] id="7BZHq45_2083"
 # ### Conclusion and Future Work
